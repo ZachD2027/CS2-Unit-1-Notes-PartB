d43036b5960f52e853e59f7dd9e72e61946c440a
Zach <zachary.dowling2027@gbwl.org>
2025-10-03 12:42:57 +0000

    1.8 Notes

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
+/**
+ * @author Zach D.
+ * @since Oct 3 2025
+ * @version 1.0  
+ * Notes for topics 1.8-1.15
+ */
 public class Main {
 
+   /**
+    * main method is the entry point of the program
+    * @param input is an array of Strings
+    * @return VOID means NO OUTPUT 
+    */
    public static void main(String []args) {
       System.out.println("Hello World");
    }
